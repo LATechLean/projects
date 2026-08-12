c5989d1c84813b3626424ebbc6545744a24bdcb0
blake-farman <blake.farman@gmail.com>
2026-08-11 20:59:39 -0500

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,22 +1,22 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "shorttitlelowercase-3",
+  "id": "projects-3",
   "level": "1",
-  "url": "shorttitlelowercase-3.html",
+  "url": "projects-3.html",
   "type": "Front Matter",
   "number": "",
   "title": "Front Matter",
-  "body": "  Blake Farman    For the midterm project, you will work in a small group to formalize and prove a result in Lean. Below are two project options. Each project will require you to formalize precise definitions and prove supporting lemmas to complete a central proof.  Your project will be graded on correctness, clarity, Lean style, and collaboration. Choose one option and submit your choice by April 4.   "
+  "body": "  Blake Farman   "
 },
 {
-  "id": "section-intro",
+  "id": "section-introduction",
   "level": "1",
-  "url": "section-intro.html",
+  "url": "section-introduction.html",
   "type": "Section",
   "number": "1",
   "title": "Introduction",
-  "body": " Introduction   For the midterm project, you will work in a small group to formalize and prove a result in Lean. Below are two project options. Each project will require you to formalize precise definitions and prove supporting lemmas to complete a central proof.  Your project will be graded on correctness, clarity, Lean style, and collaboration. Choose one option and submit your choice by April 4.   "
+  "body": " Introduction  For the midterm project, you will work in a small group to formalize and prove a result in Lean. Below are two project options. Each project will require you to formalize precise definitions and prove supporting lemmas to complete a central proof.  Your project will be graded on correctness, clarity, Lean style, and collaboration. Choose one option and submit your choice by April 4.  "
 },
 {
   "id": "sec-natural_numbers",
@@ -52,7 +52,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "3",
   "title": "Insertion Sort",
-  "body": " Insertion Sort   This project will guide you through some of the key portions of proving the correctness of the insertionSort algorithm. Along the way, you will hone the following skills   Defining recursive functions,    Pattern matching on inductive types , and    Writing proofs by induction .   In the class GitHub repository , you will find a folder named projects and within it a file insertionSort.lean that provides a code stub to get you started.    Definitions  You are given the following definition for sorted    inductive sorted {α : Type u} [LinearOrder α] : List α → Prop | nil : sorted nil | singleton (a : α) : sorted (cons a nil) | cons (a b : α) (as : List α) (a_le_b : a ≤ b) (h_sorted : sorted (cons b as)) : sorted (cons a (cons b as))   You will need to provide a definition of insert , def insert (x : α) (as : List α): List α     Basic Results  Once you have decided on a definition for insert, you should test your definition by proving the following basic results:    lemma insert_cons_le (x a : α) (as : List α) (h : x ≤ a) : insert x (cons a as) = cons x (cons a as)      lemma insert_cons_nle (x a : α) (as : List α) (h : ¬ (x ≤ a)) : insert x (cons a as) = cons a (insert x as)        Main Results  You will need to implement insertionSort  def insertionSort (as : List α) : List α recursively and then prove that actually sorts a list.    theorem insert_preserves_sorted (a : α) (as : List α) (h : sorted as) : sorted (insert a as)      theorem sort_sorts (as : List α) : sorted (insertionSort as)       "
+  "body": " Insertion Sort   This project will guide you through some of the key portions of proving the correctness of the insertionSort algorithm. Along the way, you will hone the following skills   Defining recursive functions,    Pattern matching on inductive types , and    Writing proofs by induction .   In the class GitHub repository , you will find a folder named projects and within it a file insertionSort.lean that provides a code stub to get you started.    Definitions  You are given the following definition for sorted    inductive sorted {α : Type u} [LinearOrder α] : List α → Prop | nil : sorted nil | singleton (a : α) : sorted (cons a nil) | cons (a b : α) (as : List α) (a_le_b : a ≤ b) (h_sorted : sorted (cons b as)) : sorted (cons a (cons b as))   You will need to provide a definition of insert , def insert (x : α) (as : List α): List α     Basic Results  Once you have decided on a definition for insert, you should test your definition by proving the following basic results:    lemma insert_cons_le (x a : α) (as : List α) (h : x ≤ a) : insert x (cons a as) = cons x (cons a as)      lemma insert_cons_nle (x a : α) (as : List α) (h : ¬ (x ≤ a)) : insert x (cons a as) = cons a (insert x as)        Main Results  You will need to implement insertionSort  def insertionSort (as : List α) : List α recursively and then prove that it actually sorts a list.    theorem insert_preserves_sorted (a : α) (as : List α) (h : sorted as) : sorted (insert a as)      theorem sort_sorts (as : List α) : sorted (insertionSort as)       "
 },
 {
   "id": "sec-insertion_sort-2-1",
